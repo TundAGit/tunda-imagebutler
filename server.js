77dffd5a00892bd1a71b33b06b9c40fb2e9bd0c9
JakNe <cover@livingroom-cologne.de>
2018-10-08 19:31:51 +0200

    firstasdasd35

--- server.js
+++ server.js
@@ -86,14 +86,15 @@ function request_page(url, width_1, height_1, callback){
 					var height_x = Math.round(getQueryVariable('height',page.url));
 					properties.heightx = height_x;
 					
-					
+					page.viewportSize = { width: 1400, height: height_x };
+					/*
 					page.clipRect = {
 						top: 0,
 						left: 0,
 						width: 1400,
 						height: height_x,
 					};
-					
+					*/
 					//page.clipRect = { top: 0, left: 0, width: 1400, height: 1400, };
 					
 					//page.clipRect = { top: 0, left: 0, width: 1400, height: 666, };
